23628bb8799322324b41af2e1bffa17e5ed2ebd5
Harisali78 <aliharis2739@gmail.com>
2022-12-14 07:28:16 +0500

    first commit

---
+++ pages/about-us.js
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis dolorem
+        debitis necessitatibus odit quam nam ipsum natus ea temporibus dicta?
+        Molestias officia necessitatibus voluptatum eos quae error placeat
+        facilis velit.
+      </p>
+    </div>
+  );
+};
+
+export default Page;
